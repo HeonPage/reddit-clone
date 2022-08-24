b46c2c6e503796e2885977bae8a51183c9038108
gkddyd <gkddyd@naver.com>
2022-08-24 13:58:14 +0900

    user entity

---
+++ server/src/entities/Entity.ts
@@ -0,0 +1,13 @@
+import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export default abstract class Entity extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,18 +1,41 @@
-// import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Exclude } from "class-transformer"
+import { IsEmail, Length, ValidationTypes } from "class-validator"
+import { Entity, Column, BaseEntity, Index, OneToMany, BeforeInsert } from "typeorm"
+import bcrypt from "bcryptjs"
 
-// @Entity()
-// export class User {
+@Entity("users")
+export class User extends BaseEntity {
 
-//     @PrimaryGeneratedColumn()
-//     id: number
+    @Index()
+    @IsEmail(undefined, { message: "이메일 주소가 잘못되었습니다." })
+    @Length(1, 255, { message: "이메일 주소는 비워둘수 없습니다." })
+    @Column({ unique: true })
+    email: string
 
-//     @Column()
-//     firstName: string
 
-//     @Column()
-//     lastName: string
+    @Index()
+    @Length(3, 32, { message: "사용자 이름은 3자 이상이어야 합니다." })
+    @Column({ unique: true })
+    username: string
 
-//     @Column()
-//     age: number
+    @Exclude()
+    @Column()
+    @Length(6, 255, { message: "비밀번호는 6자리 이상이어야 합니다." })
+    password: string
 
-// }
+    @OneToMany(() => postMessage, (post) => post.user)
+    posts: Post[]
+
+    @OneToMany(() => ValidationTypes, (vote) => vote.user)
+    votes: Vote[]
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 6)
+    }
+
+
+
+
+
+}
