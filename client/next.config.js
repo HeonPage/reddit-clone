498c5e8849ad937e68ad4e48ac4139f00f321165
gkddyd <gkddyd@naver.com>
2022-10-19 22:18:56 +0900

    오타2

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.gravatar.com", "localhost", "ec2-43-200-117-200.ap-northeast-2.compute.amazonaws.com"]
+    domains: ["www.gravatar.com", "localhost", "ec2-43-200-117-200.ap-northeast-2.compute.amazonaws.com", "ec2-43-200-117-200.ap-northeast-2.compute.amazonaws.com:3000"]
   }
 }
 
