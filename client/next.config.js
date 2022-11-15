814785eee5d881a0bc452ae5fb2ec44aa0f64c13
gkddyd <gkddyd@naver.com>
2022-11-15 16:44:59 +0900

    feat: add ip

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.gravatar.com", "localhost", "ec2-43-200-117-200.ap-northeast-2.compute.amazonaws.com", "ec2-43-200-117-200.ap-northeast-2.compute.amazonaws.com:3000"]
+    domains: [
+      "www.gravatar.com",
+      "localhost",
+      "ec2-43-200-117-200.ap-northeast-2.compute.amazonaws.com",
+      "ec2-43-200-117-200.ap-northeast-2.compute.amazonaws.com:3000",
+      "43.200.117.200",
+      "43.200.117.200:3000",
+    ]
   }
 }
 
